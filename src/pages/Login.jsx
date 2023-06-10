58f2bf7644815333f904682f6ab0aa50c758674d
tndevfactory <tndevfactory@gmail.com>
2023-06-10 14:13:45 +0100

    formation mern antd

---
+++ src/pages/Login.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import { Button, Checkbox, Form, Input, Card, Col, Row } from "antd";
+
+const sendBackend = (values) => {
+  console.log("send backend");
+  console.log("Success:", values);
+
+  // call backend pour verifier l'authenticité
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const Login = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "5rem",
+      }}
+    >
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 800,
+          backgroundColor: "transparent",
+          border: "solid 1px #000",
+          borderRadius: "8px",
+          padding: "1rem",
+          marginTop: "1rem",
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={sendBackend}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <h2
+          style={{
+            color: "#555",
+            backgroundColor: "transparent",
+            textAlign: "center",
+          }}
+        >
+          Login
+        </h2>
+        <Form.Item
+          label=" email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez entrer votre email",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Mot de passe"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez entrer votre mot de passe!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          style={{
+            backgroundColor: "transparent",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
